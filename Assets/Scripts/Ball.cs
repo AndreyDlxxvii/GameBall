430d05edad440f360ffa246f3c25c465893c88d1
AndreyDlxxvii <Dlxxvii@gmail.com>
2020-03-02 17:29:45 +0300

    Add player

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private float _jump;
+    private Rigidbody2D _rb;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        Jump();
+    }
+
+
+    void Update()
+    {
+        _jump = Input.GetAxis("Jump");
+        print(_jump);
+    }
+    private void Jump()
+    {
+        _rb.gravityScale = _rb.gravityScale * _jump;
+    }
+}
