4bc3bc27ed6c40a74fbba4664afcc510715e5596
AndreyDlxxvii <dlxxvii@gmail.com>
2020-03-03 22:39:59 +0300

    Add slidebar

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
-    public float Jump;
     private Rigidbody2D _rb;
+    public float Gravity;
+    private int _val;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _rb.gravityScale = Gravity;
+        
     }
     void Update()
     {
+        print(_val);
         if (Input.GetKeyDown("space"))
         {
-            _rb.gravityScale = _rb.gravityScale * -Jump;
+            _rb.gravityScale = _rb.gravityScale * -1;
         }
     }
-
+    public void SlidEvent(Slider sl)
+    {
+        _val = (int)sl.value;
+    }
 }
--- Assets/Scripts/Setting.cs
+++ Assets/Scripts/Setting.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Setting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int val;
+public void SlidEvent (Slider sl)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        val = (int)sl.value;
     }
 }
